3fc01a2fa863295b4549ffbac3ae3dac3700ffb5
bcannoe <36521191+bcannoe@users.noreply.github.com>
2018-02-19 15:11:45 -0500

    Add files via upload

---
+++ Vera/src/TestClass.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+//Blake Cannoe
+//Blake Gibson
+public class TestClass {
+	public final static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+	
+		int[] playsSlotOne = new int [31];
+		int quarters = 0;
+		int choice = 0;
+		int count = 0;
+		quarters = getQuarters(quarters);
+	
+		while(quarters > 0 && choice != 6) {
+			
+		
+		
+		
+		do {
+			
+			String message = "Enter a selection\n";
+			message += "1. Slot machine 1\n";
+			message += "2. Slot machine 2\n";
+			message += "3. Slot machine 3\n";
+			message += "4. Slot machine 4\n";
+			message += "5. Display quarters\n";
+			message += "6. Quit\n";
+			
+			System.out.println(message);
+			choice = scan.nextInt();
+			
+			
+			
+			switch(choice) {
+			case 1:
+				countSlotOne(playsSlotOne);
+				quarters = slot1(quarters, count);
+				break;
+				
+			case 2:
+				
+				break;
+				
+			case 3:
+	
+				break;
+	
+			case 4:
+	
+				break;
+	
+			case 5:
+				System.out.println(quarters);
+				break;
+				
+			case 6:
+				System.out.println("Goodbye");
+				break;
+			default:
+				System.out.println("Invalid");
+				break;
+				
+			}
+		
+			
+		
+		
+		}while(choice != 6 && quarters > 0);
+		
+	}
+	if(quarters <= 0) {
+		System.out.println("Sorry you are broke");
+	}
+		
+}
+	
+	public static int getQuarters(int quarters) {
+		System.out.println("How many quarters do you have");
+		quarters = scan.nextInt();
+		return quarters;
+		
+	}
+	
+	
+	public static int slot1(int quarters, int counts) {
+		int []plays = new int [31];
+		int quartersOut = 25;
+		counts++;
+		
+		if(plays[counts] == 31) {
+		
+		quarters += quartersOut;
+		System.out.println("you won 25 quarters");
+		
+		} else if (plays[counts] < 31) {
+		
+		quarters--;
+		}
+		
+		
+	
+		return quarters;
+	}
+	
+	public static int(int[] playsSlotOne){
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return playsSlotOne[];
+	}
+
+}
